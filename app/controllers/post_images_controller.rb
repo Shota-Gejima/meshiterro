7123d2f07639567bc04623cc64c54f8df774ff9c
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-09-14 13:56:34 +0000

    [add-5] meshiterro

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -28,6 +28,15 @@ class PostImagesController < ApplicationController
    
     @post_image = PostImage.find(params[:id])
    
+  end
+
+  def destroy
+    
+    post_image = PostImage.find(params[:id])
+    
+    post_image.destroy
+    
+    redirect_to '/post_images'
     
   end
 
